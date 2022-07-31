89484c5641e4a04a6b4e19b30a7fbb679639f062
Dario Necco <dartie90@gmail.com>
2022-07-31 20:40:21 +0200

    Built-in Handlers in urls.go file

--- urls.go
+++ urls.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func routes(r *gin.Engine) {
-	r.GET("/login", getLoginHandler)
+	// Login
+	r.GET("/login", func(c *gin.Context) { c.HTML(http.StatusOK, "login.html", nil) })
 	r.POST("/login", postLoginHandler)
+
+	// Logout
 	r.GET("/logout", getLogoutHandler)
+
+	// Register
+	r.GET("/register", func(c *gin.Context) { c.HTML(http.StatusOK, "registration.html", nil) })
 
 	protected := r.Group("/").Use(auth)
 	unprotected := r.Group("/")

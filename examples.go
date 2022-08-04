3b2db07b929616a68b7de8f3242966bacf2ed74e
Dario Necco <dnecco@perfore.com>
2022-08-04 13:20:29 +0200

    Feedback, Bootstrap 5.2, variables nested templates.
    F: templates where included without the dot at the end of include statement, which let to hide variables in nested templates.
    
    U : Bootstrap upgraded to 5.2
    
    N: Added docs.css from bootstrap, for making alerts with a proper size.
    
    N : Implemented Feedback messages.
    
    N : Created examples.go file for writing functions to test features.
    
    E : Improved Profile stuff.

---
+++ examples.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func examples(r *gin.Engine) {
+	admin := r.Group("/").Use(isAdmin)
+	protected := r.Group("/").Use(auth)
+	unprotected := r.Group("/")
+	_ = admin
+	_ = protected
+	_ = unprotected
+
+	// raise feedback alert in the template page. This is feasible as topbar.html contains the code for it.
+	unprotected.GET("/", func(c *gin.Context) {
+		Feedback := map[string]string{"Message": "1", "Message2": "2", "Info": "-1", "Well done!": "0"}
+		c.HTML(http.StatusOK, "home.html", gin.H{"Feedback": Feedback})
+	})
+}
--- urls.go
+++ urls.go
@@ -45,8 +45,52 @@ func routes(r *gin.Engine) {
 		c.HTML(http.StatusOK, "profile.html", gin.H{"User": userInfoMap, "mode": "edit"})
 	})
 
-	// Edit Profile
+	// Create Profile GET /register -> POST /create-profile
 	r.POST("/create-profile", func(c *gin.Context) {
+		userInfoMap := getCurrentUserMap(c)
+		username := c.DefaultPostForm("username", "")
+		password := c.DefaultPostForm("password", "")
+		firstName := c.DefaultPostForm("first_name", "")
+		lastName := c.DefaultPostForm("last_name", "")
+		phone := c.DefaultPostForm("mobile", "")
+		email := c.DefaultPostForm("email", "")
+		birthday := c.DefaultPostForm("birthday", "")
+		picture, _ := c.FormFile("upload_profile_pic")
+
+		var profileData []byte
+		if picture != nil {
+			file, err := picture.Open()
+			checkErr(err)
+			defer file.Close()
+			profileData, err = ioutil.ReadAll(file)
+			checkErr(err)
+		}
+
+		sqlInsertString := fmt.Sprintf(`INSERT INTO User( 
+Id, username, password, first_name, last_name, email, birthday, picture, phone, date_joined, last_login, role, is_admin, active
+)
+VALUES
+(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+`)
+		sqlCommand, err := db.Prepare(sqlInsertString)
+		checkErr(err)
+		recordId, sqlErr := sqlCommand.Exec(username, password, firstName, lastName, email, birthday, profileData, phone, "", "", "", true, true)
+
+		var message string
+		var status string
+		if sqlErr == nil {
+			message = fmt.Sprintf("User \"%s\" - ID = \"%s\" has been created successfully", username, recordId)
+			status = "success"
+		} else {
+			message = fmt.Sprintf("Issues creating user %s", username)
+			status = "danger"
+		}
+
+		c.HTML(http.StatusOK, "home.html", gin.H{"User": userInfoMap, "feedback": map[string]string{"massage": message, "status": status}})
+	})
+
+	// Edit Profile
+	r.POST("/update-profile", func(c *gin.Context) {
 		userInfoMap := getCurrentUserMap(c)
 		username := c.DefaultPostForm("username", "")
 		password := c.DefaultPostForm("password", "")

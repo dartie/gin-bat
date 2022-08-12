65426b764d7d4173f0c7a2f3767217128773ab4e
Dario Necco <dartie90@gmail.com>
2022-08-12 17:44:24 +0200

    username is UNIQUE

--- sql/initdb.sql
+++ sql/initdb.sql
@@ -4,17 +4,17 @@ DROP TABLE IF EXISTS User;
 /* Create "User" Table */ 
 CREATE TABLE User (
     id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,  
+    username TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL,
 	first_name TEXT,
-	last_name TEXT,  
-	email TEXT,     
-	birthday TEXT, 
-	picture BLOB,   
-	phone TEXT,     
+	last_name TEXT,
+	email TEXT,
+	birthday TEXT,
+	picture BLOB,
+	phone TEXT,
 	date_joined TEXT NOT NULL,
-	last_login TEXT NOT NULL, 
-	role TEXT,      
+	last_login TEXT NOT NULL,
+	role TEXT,
 	is_admin INTEGER NOT NULL,
 	active INTEGER NOT NULL
 );
@@ -23,17 +23,17 @@ CREATE TABLE User (
 /*
 CREATE TABLE User (
     Id INTEGER PRIMARY KEY,
-    Username TEXT,  
+    Username TEXT,
 	Password TEXT,
 	FirstName TEXT,
-	LastName TEXT,  
-	Email TEXT,     
-	Birthday TEXT, 
-	Picture BLOB,   
-	Phone TEXT,     
+	LastName TEXT,
+	Email TEXT,
+	Birthday TEXT,
+	Picture BLOB,
+	Phone TEXT,
 	DateJoined TEXT,
-	LastLogin TEXT, 
-	Role TEXT,      
+	LastLogin TEXT,
+	Role TEXT,
 	IsAdmin INTEGER NOT NULL,
 	Active INTEGER NOT NULL
 );
